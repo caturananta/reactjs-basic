28be58867ce354c71fb0974d86b05d557bc6e2a3
catur <caturanantaa@gmail.com>
2019-08-22 16:21:59 +0700

    first

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My First App! Yeeaaaa
         </p>
         <a
           className="App-link"
